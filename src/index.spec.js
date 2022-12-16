4721ea234f575bcba1ed678c5ae3970d881d807f
Andreas Helmberger <public@ahelmberger.de>
2022-12-16 12:59:03 +0100

    EDU-975 Create a shared ESLint config

---
+++ src/index.spec.js
@@ -0,0 +1,68 @@
+import url from 'node:url';
+import { ESLint } from 'eslint';
+import eslintConfig from './index.js';
+import { promises as fs } from 'node:fs';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+const testFilesDir = url.fileURLToPath(new URL('./test-files', import.meta.url));
+
+// As the actual test-files folder is globally ignored,
+// we pretend towards eslint that the test files actually come from here:
+const pretendtestFilesDir = url.fileURLToPath(new URL('./non-ignored-test-files', import.meta.url));
+
+describe ('index', () => {
+  let eslint;
+
+  beforeEach(() => {
+    eslint = new ESLint({ overrideConfig: eslintConfig, useEslintrc: false });
+  });
+
+  describe('when used on a file not containing any errors', () => {
+    let results;
+
+    beforeEach(async () => {
+      const sourceCode = await fs.readFile(`${testFilesDir}/test-file-without-error.js`, 'utf8');
+      results = await eslint.lintText(sourceCode, { filePath: `${pretendtestFilesDir}/test-file-without-error.js` });
+    });
+
+    it('should not report any error', () => {
+      expect(results.length).toBe(1);
+      expect(results[0].errorCount).toBe(0);
+    });
+  });
+
+  describe('when used on a file containing an error', () => {
+    let results;
+
+    beforeEach(async () => {
+      const sourceCode = await fs.readFile(`${testFilesDir}/test-file-with-error.js`, 'utf8');
+      results = await eslint.lintText(sourceCode, { filePath: `${pretendtestFilesDir}/test-file-with-error.js` });
+    });
+
+    it('should report the error', () => {
+      expect(results.length).toBe(1);
+      expect(results[0].errorCount).toBe(1);
+      expect(results[0].messages[0].severity).toBe(2);
+      expect(results[0].messages[0].ruleId).toBe('semi');
+      expect(results[0].messages[0].message).toBe('Missing semicolon.');
+    });
+  });
+
+  describe('when used on a file containing a JSX error', () => {
+    let results;
+
+    beforeEach(async () => {
+      const sourceCode = await fs.readFile(`${testFilesDir}/test-file-with-jsx-error.js`, 'utf8');
+      results = await eslint.lintText(sourceCode, { filePath: `${pretendtestFilesDir}/test-file-with-jsx-error.js` });
+    });
+
+    it('should report the error', () => {
+      expect(results.length).toBe(1);
+      expect(results[0].errorCount).toBe(1);
+      expect(results[0].messages[0].severity).toBe(2);
+      expect(results[0].messages[0].ruleId).toBe('react/jsx-max-props-per-line');
+      expect(results[0].messages[0].message).toBe('Prop `title` must be placed on a new line');
+    });
+  });
+
+});
